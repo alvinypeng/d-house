c593bd8a45c46bdc1090f47bafe9bc5a2952ba62
Alvin Peng <52754886+alvinypeng@users.noreply.github.com>
2022-01-01 14:44:53 +0000

    Update search.py

--- src/search.py
+++ src/search.py
@@ -555,7 +555,7 @@ def iterative_deepening(pos: Position, depth: int, thread_no: int,
         time_elapsed = perf_counter() - start
 
         # Print info
-        if thread_no == 0:
+        if thread_no == 0 and depth >= 10:
             info = f'info depth {d} '
             info += score_str
             info += (
